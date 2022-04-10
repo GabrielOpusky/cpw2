07b0d4788f5a6498280b8b6c4ccc8e40f9352c20
GabrielOpusky <102967160+GabrielOpusky@users.noreply.github.com>
2022-04-10 14:35:17 -0300

    Add files via upload

---
+++ questão array.js
@@ -0,0 +1,13 @@
+
+let saida = "";
+
+while(true){
+    let entrada = prompt();
+    if(entrada=="fim"){
+        break;
+    }
+    entrada +=  "    ";
+    entrada +=  saida;
+    saida    =  entrada;
+}
+alert(saida);
